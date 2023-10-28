fece5836365dcea4f8708dbc667a85623d69f00d
Leo3002 <lucasleonette@gmail.com>
2023-10-27 21:39:48 -0300

    Aula 27 10

--- lembretes/index.js
+++ lembretes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 app.use(express.json());
+const axios = require("axios");
 const lembretes = {};
 contador = 0;
 
@@ -8,14 +9,27 @@ app.get('/lembretes', (req, res) => {
     res.send(lembretes);
 });
 
-app.post('/lembretes', (req, res) => {
+app.post("/lembretes", async (req, res) => {
     contador++;
     const { texto } = req.body;
     lembretes[contador] = {
         contador, texto
     }
     res.status(201).send(lembretes[contador]);
+    await axios.post("http://localhost:10000/eventos", {
+        tipo: "LembreteCriado",
+        dados: {
+            contador,
+            texto,
+        },
+    });
+
 });
+app.post("/eventos", (req, res) => {
+    console.log(req.body);
+    res.status(200).send({ msg: "ok" });
+});
+
 
 app.listen(4000, () => {
     console.log('Lembretes. Porta 4000');
